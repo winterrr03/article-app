d667ab4d38d7ee9f73b6a36b25b6dfc88c491388
winterrr03 <dong.huynhquang21503@gmail.com>
2024-06-26 17:18:37 +0700

    create project

---
+++ index.ts
@@ -0,0 +1,34 @@
+import express, { Express, Request, Response } from "express";
+import env from "dotenv";
+env.config();
+
+import { connect } from "./config/database";
+import { ApolloServer } from "apollo-server-express";
+import { typeDefs } from "./typeDefs";
+import { resolvers } from "./resolvers";
+
+const startServer = async () => {
+  connect();
+
+  const app: Express = express();
+  const port: (number | string) = `${process.env.PORT}` || 3000;
+
+  // GraphQL
+  const apolloServer = new ApolloServer({
+    typeDefs: typeDefs,
+    resolvers: resolvers
+  });
+
+  await apolloServer.start();
+
+  apolloServer.applyMiddleware({
+    app: app,
+    path: "/graphql"
+  })
+
+  app.listen(port, () => {
+    console.log(`App listening on port ${port}`);
+  });
+}
+
+startServer();
